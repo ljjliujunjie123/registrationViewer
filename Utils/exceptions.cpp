148a2e324ce69b0d6bc079e656089acfbcdde9a1
RandomNamer <zzyatgm@gmail.com>
2021-12-09 14:31:23 +0800

    std::execption compat fix

--- Utils/exceptions.cpp
+++ Utils/exceptions.cpp
@@ -5,7 +5,14 @@
 #include<exception>
 
 class NotFoundException: std::exception{
+#ifdef __clang__
     const char *what() const _NOEXCEPT override {
         return "The requested item cannot be found";
     }
+#endif
+#ifdef _WIN32
+    const char *what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW override {
+        return "The requested item cannot be found";
+    }
+#endif
 };
