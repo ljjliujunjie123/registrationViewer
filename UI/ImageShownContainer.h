ff989e857641cb6c5c982dc7d89002ad19da0fef
ljjliujunjie123 <ljjliujunjie123@163.com>
2021-12-16 16:14:43 +0800

    UI初步设计

---
+++ UI/ImageShownContainer.h
@@ -0,0 +1,19 @@
+//
+// Created by 19956875375 on 2021/12/15.
+//
+
+#ifndef REGISTRATIONVIEWER_IMAGESHOWNCONTAINER_H
+#define REGISTRATIONVIEWER_IMAGESHOWNCONTAINER_H
+
+#include <QFrame>
+#include <QSize>
+
+class ImageShownContainer: public QFrame {
+
+public:
+    ImageShownContainer(QWidget *parent = nullptr);
+
+};
+
+
+#endif //REGISTRATIONVIEWER_IMAGESHOWNCONTAINER_H
